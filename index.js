12589f98e95be45b74a4c7de8bf77a6fefda4f68
Alex Costan <acostan.con@orangetheory.com>
2020-09-16 16:19:07 -0400

    creating a simple web server

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const { Console } = require("console")
 const fs = require("fs")
+const http = require('http')
+
+//////////////////////////
+// FILES
 
 // Blocking, synchronous way
-// const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
-// console.log(textIn)
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textIn)
 
-// const textOut = `{This is what we know about the avocado: ${textIn}}.\nCreated on ${Date.now()}`
-// fs.writeFileSync('./txt/output.txt', textOut)
-// console.log('File written!')
+const textOut = `{This is what we know about the avocado: ${textIn}}.\nCreated on ${Date.now()}`
+fs.writeFileSync('./txt/output.txt', textOut)
+console.log('File written!')
 
 // Non-blocking, asynchronous way
 fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
@@ -26,3 +30,13 @@ fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
 // The Triangle shape of the fs code above is a tel-tale sign of callback hell
 
 console.log("Will print to the console before async code!")
+
+/////////////////////////////
+// SERVER
+const server = http.createServer((req, res) => {
+    res.end('Hello from the server!')
+})
+
+server.listen(8000, '127.0.0.1', () => {
+    console.log('Listening to requests on port 8000')
+})
