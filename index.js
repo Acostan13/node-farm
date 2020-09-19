4cb57552a95297b60677f011b52be9639489101a
Alex Costan <acostan.con@orangetheory.com>
2020-09-19 14:50:08 -0400

    creating slugs for neater urls

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require("fs")
 const http = require("http")
 const url = require("url")
-
+const slugify = require('slugify')
 const replaceTemplate = require('./modules/replaceTemplate')
 
 //////////////////////////
@@ -43,9 +43,14 @@ const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.htm
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8")
 const dataObj = JSON.parse(data)
 
+const slugs = dataObj.map(el => slugify(el.productName, {lower: true}))
+console.log(slugs)
+
+console.log(slugify('Fresh Avocados', {lower: true}))
+
 const server = http.createServer((req, res) => {
   const {query, pathname} = url.parse(req.url, true)
-  console.log(pathname)
+
   // Overview page
   if (pathname === "/overview" || pathname === "/") {
     res.writeHead(200, {'Content-type': 'text/html'})
